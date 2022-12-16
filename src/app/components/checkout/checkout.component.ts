4b2e3fa30d7a8fa63e6295c3be4b495c7f0ad1a7
Youssef Nafaa <nafaa.youssef93@gmail.com>
2022-12-16 16:01:45 +0100

    environment configuration frontend

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -122,7 +122,8 @@ export class CheckoutComponent implements OnInit {
         lastName: new FormControl('', [Validators.required,
                                        Validators.minLength(2),
                                        Luv2ShopValidators.notOnlyWhitespace]),
-        email: new FormControl(theEmail,
+        email: new FormControl(theEmail
+          ,
         [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
       }),
       shippingAddress: this.formBuilder.group({
--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
     oidc:{
         clientId: '0oa7lnv9z1POlQUrG5d7',
         issuer: 'https://dev-78494002.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 }
